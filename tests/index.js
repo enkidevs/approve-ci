d5f0abcab7da8c5cde5ebfc9439a382ff21fbaf9
lukem512 <lukem95@gmail.com>
2016-04-20 17:51:26 +0100

    Initial testing

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,21 @@
 import test from 'ava'
 
-test.todo('write tests')
+import {testIfHookAlreadyExist, mergeConfigs, checkApproved} from '../src/approval'
+
+const comments = require('./fixtures/comments.json')
+const content = require('./fixtures/content.json')
+const hooks = require('./fixtures/hooks.json')
+const pr = require('./fixtures/pr.json')
+
+test('Check for existing web hook', t => t.throws(testIfHookAlreadyExist(hooks)))
+
+test('Check configs merge', t => {
+  t.same(mergeConfigs(content), {
+    name: 'approve-test',
+    approvalCount: 1,
+    approvalStrings: ['👍', ':+1:', ':thumbsup:'],
+    disapprovalStrings: ['👎', ':-1:', ':thumbsdown:']
+  })
+})
+
+test('Check request is approved', t => t.true(checkApproved(comments, pr)))
