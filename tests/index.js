1a898be5bfda0766ec72321a4e13f341241e8479
Mathieu Dutour <mathieu@dutour.me>
2016-04-20 18:05:16 +0100

    test passing

--- tests/index.js
+++ tests/index.js
@@ -7,10 +7,17 @@ const content = require('./fixtures/content.json')
 const hooks = require('./fixtures/hooks.json')
 const pr = require('./fixtures/pr.json')
 
-test('Check for existing web hook', t => t.throws(testIfHookAlreadyExist(hooks)))
+test('Check for existing web hook', t => {
+  try {
+    testIfHookAlreadyExist(hooks)
+    t.fail()
+  } catch (e) {
+    t.pass()
+  }
+})
 
 test('Check configs merge', t => {
-  t.same(mergeConfigs(content), {
+  return t.deepEqual(mergeConfigs(content), {
     name: 'approve-test',
     approvalCount: 1,
     approvalStrings: ['👍', ':+1:', ':thumbsup:'],
@@ -18,4 +25,6 @@ test('Check configs merge', t => {
   })
 })
 
-test('Check request is approved', t => t.true(checkApproved(comments, pr)))
+test('Check request is approved', t => {
+  return t.truthy(checkApproved([comments, pr]))
+})
