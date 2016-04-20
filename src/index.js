6d88d6770e30b72ce12e63fc113595c83627b3ea
lukem512 <lukem95@gmail.com>
2016-04-20 11:47:34 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log('hello world')
+import express from 'express'
+import GithubAPI from 'github'
+
+const {GITHUB_TOKEN, GITHUB_REPO, GITHUB_ORG, URL} = process.env
+
+var headers = {
+  'user-agent': 'approve-ci-bot'
+}
+
+var gh = new GithubAPI({
+  // required
+  version: '3.0.0',
+  // optional
+  debug: true,
+  protocol: 'https',
+  host: 'api.github.com',
+  pathPrefix: '',
+  timeout: 5000,
+  headers: headers
+})
+
+gh.authenticate({
+  type: 'oauth',
+  token: GITHUB_TOKEN
+})
+
+gh.getHooks({
+  user: GITHUB_ORG,
+  repo: GITHUB_REPO,
+  headers: headers
+}, (response) => {
+  console.log(response)
+})
+
+const app = express()
+
+app.get('/', (res, req) => {
+  res.send('Hello, world!')
+})
+
+app.post('/', (res, req) => {
+  // TODO
+})
